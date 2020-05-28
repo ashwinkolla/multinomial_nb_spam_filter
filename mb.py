d300c7f34527228cd9fef004749c7e59c16a294b
Ashwin Kolla <ashwinkolla@Ashwins-MacBook-Pro.local>
2020-05-27 19:41:37 -0500

    Creating methods to improve code style

--- mb.py
+++ mb.py
@@ -31,56 +31,43 @@ def dataFrameFromDirectory(path, classification):
 
     return DataFrame(rows, index=index)
 
-data = DataFrame({'message': [], 'class': []})
-data = data.append(dataFrameFromDirectory('C:/Users/Ashwin/Documents/EnronDataSets/enron1/spam', 'spam'), sort=True)
-data = data.append(dataFrameFromDirectory('C:/Users/Ashwin/Documents/EnronDataSets/enron1/ham', 'ham'), sort=True)
-vectorizer = CountVectorizer()
-counts = vectorizer.fit_transform(data['message'].values)
-classifier = MultinomialNB()
-targets = data['class'].values
-classifier.fit(counts, targets)
+
+def build_model(spam_dict):
+    data = DataFrame({'message': [], 'class': []})
+    for classif in spam_dict:
+        for train_data_path in spam_dict[classif]:
+            data = data.append(dataFrameFromDirectory(train_data_path, classif), sort=True)
+    vectorizer = CountVectorizer()
+    counts = vectorizer.fit_transform(data['message'].values)
+    classifier = MultinomialNB()
+    targets = data['class'].values
+    classifier.fit(counts, targets)
+    return vectorizer, classifier
 
 
-numSpamMessages = 0
-spamCount = 0
-testSpamPath = 'C:/Users/Ashwin/Documents/EnronDataSets/enron2/spam'
-for root, dirnames, filenames in os.walk(testSpamPath):
-    for filename in filenames:
-        path = os.path.join(root,filename)
-        numSpamMessages += 1
-        lines = []
-        f = io.open(path, 'r', encoding='latin1')
-        for line in f:
-            lines.append(line)
-        f.close()
-        message = '\n'.join(lines)
-        examples = [message]
-        example_counts = vectorizer.transform(examples)
-        predictions = classifier.predict(example_counts)
-        if(predictions[0] == "spam"):
-            spamCount += 1
-print("The actual count of spam was: " + str(spamCount))
-print("The expected count of spam was: " + str(numSpamMessages))
-print("The percentage of spam caught was: " + str(spamCount/numSpamMessages))
+def test_model(spam_test, test_path):
+    spam_dict = {}  #path for files for model to be trained on with classification goes here
+    vectorizer, classifier = build_model(spam_dict)
+    num_messages = 0
+    correct_count = 0
+    for root, dirnames, filenames in os.walk(test_path):
+        for filename in filenames:
+            path = os.path.join(root,filename)
+            num_messages += 1
+            lines = []
+            f = io.open(path, 'r', encoding='latin1')
+            for line in f:
+                lines.append(line)
+            f.close()
+            message = '\n'.join(lines)
+            examples = [message]
+            example_counts = vectorizer.transform(examples)
+            predictions = classifier.predict(example_counts)
+            if(predictions[0] == spam_test):
+                correct_count += 1
 
-numHamMessages = 0
-hamCount = 0
-testHamPath = 'C:/Users/Ashwin/Documents/EnronDataSets/enron2/ham'
-for root, dirnames, filenames in os.walk(testHamPath):
-    for filename in filenames:
-        path = os.path.join(root,filename)
-        numHamMessages += 1
-        lines = []
-        f = io.open(path, 'r', encoding='latin1')
-        for line in f:
-            lines.append(line)
-        f.close()
-        message = '\n'.join(lines)
-        examples = [message]
-        example_counts = vectorizer.transform(examples)
-        predictions = classifier.predict(example_counts)
-        if(predictions[0] == "ham"):
-            hamCount += 1
-print("The actual count of ham was: " + str(hamCount))
-print("The expected count of ham was: " + str(numHamMessages))
-print("The percentage of ham caught was: " + str(hamCount/numHamMessages))
+    print(f"\n{spam_test} Count:")            
+    print(f"The actual count of {spam_test} was: " + str(correct_count))
+    print(f"The expected count of {spam_test} was: " + str(num_messages))
+    print(f"The percentage of {spam_test} caught was: " + str(correct_count/num_messages))
+
